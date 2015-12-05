9a28950425f1ccee02e2ade2f70f1e31d80b902a
aluthens <aluthens@c-lnx001.engr.uiowa.edu>
2015-12-05 12:53:23 -0600

    test

--- RepoTest/src/Test.java
+++ RepoTest/src/Test.java
@@ -3,5 +3,6 @@ public class Test {
 	public static void main(String[] args){
 		System.out.println("test");
 		System.out.println("test2");
+		System.out.println("test: Saturday 12/05/15, 12:52 pm");
 	}
 }
