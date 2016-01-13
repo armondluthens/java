a1828be9db0353863fcf301652fb2c9ec8971f3c
Armond Luthens <armondluthens@gmail.com>
2016-01-13 11:09:33 -0600

    rwe

--- RepoTest/src/Test.java
+++ RepoTest/src/Test.java
@@ -2,7 +2,7 @@
 public class Test {
 	public static void main(String[] args){
 		System.out.println("test");
-		System.out.println("test2");
+		System.out.println("test2asdas3");
 		System.out.println("test: Saturday 12/05/15, 12:52 pm");
 	}
 }
